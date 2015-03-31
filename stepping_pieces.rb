64bbe70501d754306f34a6af3326f6244206dc63
Charles + Emmanuel <charles+emmanuel@appacademy.io>
2015-03-31 15:45:09 -0400

    Add Pawn Class and moves method.

--- stepping_pieces.rb
+++ stepping_pieces.rb
@@ -1,8 +1,71 @@
 require_relative("base_classes.rb")
 
-class Stepping < Pieces
-  def moves(dirs)
-    #array of positions piece can slide to
-    #making sure position is not occupied with friendly
+class SteppingPieces < Pieces
+  def moves(deltas)
+    row = @position.first
+    col = @position.last
+    poss_dest = []
+    deltas.each do |dx, dy|
+      if (row + dx).between?(0, 7) && (col + dy).between?(0, 7)
+        poss_dest << [row + dx, col + dy]
+      end
+    end
+
+    poss_dest
+  end
+end
+
+class Knight < SteppingPieces
+  DELTAS = [
+    [-2, -1],
+    [-2,  1],
+    [-1, -2],
+    [-1,  2],
+    [ 1, -2],
+    [ 1,  2],
+    [ 2, -1],
+    [ 2,  1]
+  ]
+
+  def moves(deltas = DELTAS)
+    super
+  end
+end
+
+class King < SteppingPieces
+  DELTAS = [
+    [ 1,  0],
+    [ 1,  1],
+    [ 1, -1],
+    [ 0,  1],
+    [ 0, -1],
+    [-1,  0],
+    [-1,  1],
+    [-1, -1],
+  ]
+  def moves(delta = DELTAS)
+    super
+  end
+end
+
+class Pawn < SteppingPieces
+  DELTAS = [
+    [ 1,  0],
+    [ 1,  1],
+    [ 1, -1],
+    [-1,  0],
+    [-1,  1],
+    [-1, -1],
+  ]
+
+  def moves
+    delta = DELTAS
+    if color == :w
+      delta = DELTAS.drop(3)
+    else
+      delta = DELTAS.take(3)
+    end
+
+    super(delta)
   end
 end
