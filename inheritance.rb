4785567e1b76f68b1cf064f1782bb9ada434bfbe
Charles + Emmanuel <charles+emmanuel@appacademy.io>
2015-03-31 10:56:59 -0400

    Fix employee-manager relationships.

--- inheritance.rb
+++ inheritance.rb
@@ -3,7 +3,26 @@ class Employee
   attr_accessor :name, :title, :salary, :boss
 
     def initialize (name, title, salary, boss=nil)
-      
+      @name = name
+      @title = title
+      @salary = salary
+      @boss = boss
     end
+
+end
+
+class Manager < Employee
+
+  attr_accessor :subordinates
+
+  def initialize(name, title, salary)
+    super(name, title, salary)
+    @subordinates = []
+  end
+
+  def add_subordinate(employee)
+    employee.boss = self
+    @subordinates << employee
+  end
 
 end
